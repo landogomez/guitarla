faae8c02b4ebaa8697fb115d7b31882b1d204a76
jorge <esgerry81@gmail.com>
2024-09-15 23:43:35 -0600

    Agregando la parte visual del carrito

--- src/comoponents/Header.jsx
+++ src/comoponents/Header.jsx
@@ -1,7 +1,7 @@
 //import { Fragment } from "react"
 //import React from "react"
 
-export default function Header(){
+export default function Header({cart}){
     //En esta seccion van los state o funciones
 
 

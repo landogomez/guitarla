7c28cbff51194d6bc5c4faa7e0b0ec5e3f01ac04
jorge <esgerry81@gmail.com>
2024-09-16 00:23:05 -0600

    Se utilizo useMemo para un mejor rendimiento

--- src/comoponents/Header.jsx
+++ src/comoponents/Header.jsx
@@ -1,10 +1,13 @@
 //import { Fragment } from "react"
 //import React from "react"
 
+import { useMemo } from "react"
+
 export default function Header({cart}){
     //En esta seccion van los state o funciones
-    const isEmpty = () => cart.length === 0
-    const cartTotal = () => cart.reduce( (total, item) => total + (item.quantity * item.price), 0)
+    const isEmpty = useMemo( () => cart.length === 0, [cart])
+    const cartTotal = useMemo(() => cart.reduce( (total, item) => total + (item.quantity * item.price), 0),
+    [cart])
 
 
     return(
@@ -28,7 +31,7 @@ export default function Header({cart}){
                         <img className="img-fluid" src="./public/img/carrito.png" alt="imagen carrito" />
 
                         <div id="carrito" className="bg-white p-3">
-                            {isEmpty() ? (
+                            {isEmpty ? (
                                 <p className="text-center">El carrito esta vacio</p>
                             ) : (
                             <>    
@@ -82,7 +85,7 @@ export default function Header({cart}){
                                         </tbody>
                                     </table>
 
-                                <p className="text-end">Total pagar: <span className="fw-bold">${cartTotal()}</span></p>
+                                <p className="text-end">Total pagar: <span className="fw-bold">${cartTotal}</span></p>
                              </>
                         )}
                          <button className="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
